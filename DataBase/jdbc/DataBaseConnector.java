42990036e70b89ccbe17be6c1c999c71f8f29e92
flokaptei <florian.kapteina2@gmail.com>
2016-09-22 17:08:23 +0200

    U

--- DataBase/jdbc/DataBaseConnector.java
+++ DataBase/jdbc/DataBaseConnector.java
@@ -1,5 +1,32 @@
 package jdbc;
 
-public class DataBaseConnector {
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
+public class DataBaseConnector {
+	{
+
+	}
+
+	Connection connection;
+
+	public DataBaseConnector(String dbIP, String dbName, String dbPort, String username, String password)
+			throws SQLException {
+
+		String connectionstring = "jdbc:mysql://";
+		if(dbIP == null || dbIP.equals("")){
+			connectionstring+="127.0.0.1:";
+		}else{
+			connectionstring+=dbIP+":";
+		}
+		if(dbPort == null || dbPort.equals("")){
+			connectionstring+="3306/"+dbName;
+		}else{
+			connectionstring+=dbPort+"/"+dbName;
+		}
+		connection = DriverManager.getConnection(connectionstring,username,password);
+		
+
+	}
 }
