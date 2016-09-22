57b28dc51c0b783f93e7b3a84dcc68cad499b396
flokaptei <florian.kapteina2@gmail.com>
2016-09-22 17:24:56 +0200

    -

--- DataBase/jdbc/DataBaseConnector.java
+++ DataBase/jdbc/DataBaseConnector.java
@@ -2,11 +2,20 @@ package jdbc;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
 import java.sql.SQLException;
+import java.sql.Statement;
+
 
 public class DataBaseConnector {
 	{
-
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 
 	Connection connection;
@@ -15,18 +24,55 @@ public class DataBaseConnector {
 			throws SQLException {
 
 		String connectionstring = "jdbc:mysql://";
-		if(dbIP == null || dbIP.equals("")){
-			connectionstring+="127.0.0.1:";
-		}else{
-			connectionstring+=dbIP+":";
+		if (dbIP == null || dbIP.equals("")) {
+			connectionstring += "127.0.0.1:";
+		} else {
+			connectionstring += dbIP + ":";
 		}
-		if(dbPort == null || dbPort.equals("")){
-			connectionstring+="3306/"+dbName;
-		}else{
-			connectionstring+=dbPort+"/"+dbName;
+		if (dbPort == null || dbPort.equals("")) {
+			connectionstring += "3306/" + dbName;
+		} else {
+			connectionstring += dbPort + "/" + dbName+"?autoreconnect=true&useSSL=false";
 		}
-		connection = DriverManager.getConnection(connectionstring,username,password);
-		
+		connection = DriverManager.getConnection(connectionstring, username, password);
 
+	}
+
+	public String executeQuery(String Query) {
+
+		try {
+			Statement statement = connection.createStatement();
+			ResultSet resultset = statement.executeQuery(Query);
+			ResultSetMetaData resultsetmetadata = resultset.getMetaData();
+
+			String out = "";
+			while (resultset.next()) {
+				for (int i = 1; i <= resultsetmetadata.getColumnCount(); i++) {
+					out += resultset.getString(i) + "    ";
+				}
+				out += "\n";
+			}
+
+			statement.close();
+			resultset.close();
+			return out;
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			e.getErrorCode();
+			e.getSQLState();
+			return null;
+		}
+
+	}
+	
+	public static void main(String[] args) {
+		try {
+			DataBaseConnector d = new DataBaseConnector("127.0.0.1", "test", "3306", "flo", null);
+			System.out.println(d.executeQuery("Select * From kunden"));
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 }
